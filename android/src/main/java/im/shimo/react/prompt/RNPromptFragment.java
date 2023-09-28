82392184854f5012216532e429cd726b725d0188
usama23sep <56351501+usama23sep@users.noreply.github.com>
2023-09-28 18:57:46 +0500

    Update RNPromptFragment.java

--- android/src/main/java/im/shimo/react/prompt/RNPromptFragment.java
+++ android/src/main/java/im/shimo/react/prompt/RNPromptFragment.java
@@ -5,7 +5,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
